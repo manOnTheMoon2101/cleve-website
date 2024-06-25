79bdf0bac78c70ba494b4a146dcdd571b60585de
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-06-25 16:54:08 +0200

    added tailwind

--- app/components/body/Welcome.tsx
+++ app/components/body/Welcome.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import cleve from "../../../public/images/Cleve.png";
-import styles from "../../page.module.css"
+import styles from "../../page.module.css";
 import Image from "next/image";
 import { FaLinkedin } from "react-icons/fa";
 import { FaArrowDown } from "react-icons/fa";
 const Welcome = () => {
   return (
     <div>
-      <div style={{ float: "left", width: "50%", textAlign: "center" }}>
+      <div>
         <Image alt="Cleve" src={cleve} />
       </div>
 
-      <div style={{ float: "left", width: "50%" }}>
-        <h2 style={{ fontSize: "60px", fontWeight: "900" }}>npm init?</h2>
+      <div>
+        <h2>npm init?</h2>
         <p style={{ fontSize: "24px", fontWeight: "400" }}>
           i make apps
           <br />
           (apps that look good btw)
         </p>
         <p style={{ fontSize: "24px" }}>
-          checkout my personal projects that i've worked on <FaArrowDown className={styles.arrowDown} />
+          checkout my personal projects that i've worked on{" "}
+          <FaArrowDown className={styles.arrowDown} />
         </p>
       </div>
       <div style={{ float: "left", width: "50%" }}>

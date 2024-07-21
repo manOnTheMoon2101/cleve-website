643774597f4fbab861c95aff798d9a631158d638
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-21 14:27:01 +0200

    added footer

---
+++ app/components/layout/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>Footer </div>
+      <div>@2024 </div>
+      <div>made by @cleveclayton</div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import TechStack from "./components/techStack/TechStack";
 import Welcome from "./components/body/welcome/Welcome";
 import Projects from "./components/body/projects/Projects";
+import Footer from "./components/layout/footer/Footer";
 
 export default function Home() {
   return (
@@ -11,11 +12,20 @@ export default function Home() {
       <div className="m-1 flex flex-col items-center justify-center min-h-screen">
         <Header />
       </div>
-      <div style={{position:'relative',top:'200px'}}>
+      <div className="relative top-32">
+        {" "}
+        {/* 200px */}
         <Welcome />
       </div>
-      <div  style={{position:'relative',top:'400px'}}>
+      <div className="relative top-64">
+        {" "}
+        {/* 400px */}
         <Projects />
+      </div>
+      <div className="relative top-64">
+        {" "}
+        {/* 400px */}
+        <Footer />
       </div>
     </main>
   );

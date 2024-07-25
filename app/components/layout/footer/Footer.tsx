77e774d644d80b799bc1489c843b571eb06215f8
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-25 18:00:44 +0200

    styled footer

--- app/components/layout/footer/Footer.tsx
+++ app/components/layout/footer/Footer.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-row justify-around">
-      <div>Footer </div>
-      <div>@2024 </div>
-      <div>made by @cleveclayton</div>
+    <div className="flex flex-col ">
+      <div className="flex flex-row justify-center">
+        <h3>Linkedin</h3>
+        <h3>Github</h3>
+      </div>
+
+      <div className="flex row justify-between">
+        <div>@2024 </div>
+        <div>made by @cleveclayton</div>
+      </div>
     </div>
   );
 };

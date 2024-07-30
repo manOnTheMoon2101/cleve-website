b2e4ea64ed0522a32bef997f01719fbabf35d345
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-30 17:06:45 +0200

    removes techstack and other shit

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,20 +1,20 @@
+"use client";
+
 import React from "react";
 import Spline from "@splinetool/react-spline/next";
 const Header = () => {
   return (
-<div className="m-1 flex flex-col items-center justify-center min-h-screen relative bg-custom-gradient ">
-  <div className="absolute top-0 left-20 right-0 bottom-0 z-10">
-    <Spline scene="https://prod.spline.design/ecIuHhpQ4E2RUhp2/scene.splinecode" />
-  </div>
+    <div className="flex flex-col items-center justify-center min-h-screen relative ">
+      <div className="absolute top-0 left-20 right-0 bottom-0 z-10">
+        <Spline scene="https://prod.spline.design/ecIuHhpQ4E2RUhp2/scene.splinecode" />
+      </div>
 
-  <h1 className="text-6xl font-bold  relative">
-    Cleve Clayton
-  </h1>
-  <h3 className="text-sm relative">
-    web designer <span>x</span> full stack developer <span>x</span> ux
-    designer
-  </h3>
-</div>
+      <h1 className="text-6xl font-bold  relative">Cleve Clayton</h1>
+      <h3 className="text-sm relative mt-2">
+        web designer <span className="text-neutral-600">x</span> full stack
+        developer <span className="text-neutral-600">x</span> ux designer
+      </h3>
+    </div>
   );
 };
 

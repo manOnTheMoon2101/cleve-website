424b812f03fb282d9e7d068cc7ab02f5ca689a9e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-31 18:06:39 +0200

    removed code from projects

--- app/components/body/projects/Projects.tsx
+++ app/components/body/projects/Projects.tsx
@@ -39,45 +39,45 @@ const Projects = () => {
           alt="spline 3d sun asset"
         />
       ),
-      code: (
-        <CopyBlock
-          text={`
-export const GET = async (
-  requests: any,
-  { params }: { params: { town: string } }
-) => {
-  try {
-    const { town } = params;
-    const apiKey = process.env.API_KEY;
-    const res = await fetch(
-      https://api.openweathermap.org/data/2.5/weather?q=\${town}&units=metric&appid=\${apiKey},
-      {
-        next: { revalidate: 900 },
-      }
-    );
+      //       code: (
+      //         <CopyBlock
+      //           text={`
+      // export const GET = async (
+      //   requests: any,
+      //   { params }: { params: { town: string } }
+      // ) => {
+      //   try {
+      //     const { town } = params;
+      //     const apiKey = process.env.API_KEY;
+      //     const res = await fetch(
+      //       https://api.openweathermap.org/data/2.5/weather?q=\${town}&units=metric&appid=\${apiKey},
+      //       {
+      //         next: { revalidate: 900 },
+      //       }
+      //     );
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
+      //     if (!res.ok) {
+      //       throw new Error("Failed to fetch data");
+      //     }
 
-    const data = await res.json();
+      //     const data = await res.json();
 
-    return Response.json(data);
-  } catch (err) {
-    return NextResponse.json(
-      {
-        message: "GET Error",
-        err,
-      },
-      { status: 500 }
-    );
-  }
-};
-  `}
-          language={"typescript"}
-          theme={dracula}
-        />
-      ),
+      //     return Response.json(data);
+      //   } catch (err) {
+      //     return NextResponse.json(
+      //       {
+      //         message: "GET Error",
+      //         err,
+      //       },
+      //       { status: 500 }
+      //     );
+      //   }
+      // };
+      //   `}
+      //           language={"typescript"}
+      //           theme={dracula}
+      //         />
+      //       ),
       info: "Basic Weather API",
       misc: "API-WEB-2024",
       techStack: "NextJS | OpenWeatherAPI | cheerio | shadcn/ui | TailwindCSS",
@@ -105,54 +105,54 @@ export const GET = async (
       descriptionFirst: "Weight Management App displays relevant data.",
       descriptionSecond: "Used Prisma as the ORM for the REST APIs",
       misc: "DASHBOARD-WEB-2024",
-      code: (
-        <CopyBlock
-          text={`
-export const GET = async (
-  req: any,
-  { params }: { params: { month: string } }
-) => {
-  try {
-    const { month } = params;
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    let session: any;
-    session = await getServerSession(authOptions);
+      //       code: (
+      //         <CopyBlock
+      //           text={`
+      // export const GET = async (
+      //   req: any,
+      //   { params }: { params: { month: string } }
+      // ) => {
+      //   try {
+      //     const { month } = params;
+      //     const currentDate = new Date();
+      //     const currentYear = currentDate.getFullYear();
+      //     let session: any;
+      //     session = await getServerSession(authOptions);
 
-    let user: any;
-    user = await prisma.user.findUnique({
-      where: {
-        email: session?.user?.email,
-      },
-    });
-    const data = await prisma.data.findMany({
-      where: {
-        userId: user.id,
-        createdAt: {
-          gte: new Date({currentYear}-{month}-01T01:00:00.459+00:00),
-          lte: new Date({currentYear}-{month}-31T01:00:00.459+00:00),
-        },
-      },
-    });
+      //     let user: any;
+      //     user = await prisma.user.findUnique({
+      //       where: {
+      //         email: session?.user?.email,
+      //       },
+      //     });
+      //     const data = await prisma.data.findMany({
+      //       where: {
+      //         userId: user.id,
+      //         createdAt: {
+      //           gte: new Date({currentYear}-{month}-01T01:00:00.459+00:00),
+      //           lte: new Date({currentYear}-{month}-31T01:00:00.459+00:00),
+      //         },
+      //       },
+      //     });
 
-    data.map((x: any) => (x.createdAt = x.createdAt.toDateString()));
+      //     data.map((x: any) => (x.createdAt = x.createdAt.toDateString()));
 
-    return NextResponse.json(data);
-  } catch (err) {
-    return NextResponse.json(
-      {
-        message: "GET filter month Error",
-        err,
-      },
-      { status: 500 }
-    );
-  }
-};
-  `}
-          language={"typescript"}
-          theme={dracula}
-        />
-      ),
+      //     return NextResponse.json(data);
+      //   } catch (err) {
+      //     return NextResponse.json(
+      //       {
+      //         message: "GET filter month Error",
+      //         err,
+      //       },
+      //       { status: 500 }
+      //     );
+      //   }
+      // };
+      //   `}
+      //           language={"typescript"}
+      //           theme={dracula}
+      //         />
+      //       ),
     },
   };
   return (
@@ -180,50 +180,25 @@ export const GET = async (
               </div>
             </DialogTrigger>
             <DialogContent className="max-w-fit overflow-y-scroll max-h-screen">
-              <DialogHeader>
+              <div>
                 <DialogTitle className="text-center text-4xl font-bold">
                   {x.title}
                 </DialogTitle>
-                <div className="flex flex-row justify-between">
-                  <DialogDescription>{x.techStack}</DialogDescription>
-                  <DialogDescription>@2024</DialogDescription>
-                </div>
-                <Separator className="my-4" />
-                <DialogDescription>
-                  <a
-                    className="underline text-cyan-400 flex flex-row justify-center"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={x.link ? x.link : ""}
-                  >
-                    {x.link ? x.link : ""}
-                  </a>
-                </DialogDescription>
-              </DialogHeader>
-              <DialogDescription>
-                {/* <div className="flex flex-row justify-center">
-                  {x.imageFirst}
-                </div> */}
+              </div>
+              <div>
+                <p className="text-center">{x.techStack}</p>
 
-                <div className="text-center text-2xl">
-                  <h3>{x.descriptionFirst}</h3>
-                </div>
-              </DialogDescription>
-
-              <DialogDescription>
-                {/* <div className="text-left text-2xl">
-                  {x.descriptionSecond}
-                </div> */}
-
-                <div>{x.code}</div>
-              </DialogDescription>
-              <DialogFooter className="sm:justify-end">
-                <DialogClose asChild>
-                  <Button type="button" variant="secondary">
-                    Close
-                  </Button>
-                </DialogClose>
-              </DialogFooter>
+                <a
+                  className="underline text-cyan-400 flex flex-row justify-center text-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={x.link ? x.link : ""}
+                
+                >
+                  {x.link ? x.link : ""}
+                </a>
+              </div>
+              <Separator className="my-4" />
             </DialogContent>
           </Dialog>
         ))}

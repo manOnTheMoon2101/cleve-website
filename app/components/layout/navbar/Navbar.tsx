d83c297e151d1f89ab8d16b264f8c065c0d47bff
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-09-17 21:48:39 +0200

    added all urls

--- app/components/layout/navbar/Navbar.tsx
+++ app/components/layout/navbar/Navbar.tsx
@@ -1,16 +1,32 @@
 "use client";
 import React from "react";
 import { ModeToggle } from "../../header/provider/Button";
-
+import Link from "next/link";
+import { FaHome } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className="sticky top-0 z-10">
       <div className="flex flex-row justify-between">
         <ModeToggle />
-        <ul className="flex flex-row">
-          <li className="mx-5">about</li>
-          <li className="mx-5">projects</li>
-          <li className="mx-5">socials</li>
+        <ul className="flex flex-row items-baseline">
+          <li className="mx-10 flex flex-row">
+            {" "}
+            <Link href="/">
+              <FaHome size={25} />
+            </Link>
+          </li>
+          <li className="mx-5">
+            {" "}
+            <Link href="/pages/about">about</Link>
+          </li>
+          <li className="mx-5">
+            {" "}
+            <Link href="/pages/projects">projects</Link>
+          </li>
+          <li className="mx-5">
+            {" "}
+            <Link href="/pages/socials">socials</Link>
+          </li>
         </ul>
       </div>
     </div>
---
+++ app/pages/about/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Footer from "../../components/layout/footer/Footer";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+import Navbar from "../../components/layout/navbar/Navbar";
+import { motion, useScroll } from "framer-motion";
+interface SectionProps {
+  children: React.ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({ children }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section ref={ref}>
+      <span
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  );
+};
+
+export default function About() {
+  const { scrollYProgress } = useScroll();
+  return (
+    <>
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-2.5 bg-emerald-400 rounded transform origin-left"
+        style={{ scaleX: scrollYProgress }}
+      />
+      <main className="m-5">
+        <Navbar />
+        <div className="mt-96  ">
+          <Section>
+            <Footer />
+          </Section>
+        </div>
+      </main>
+    </>
+  );
+}

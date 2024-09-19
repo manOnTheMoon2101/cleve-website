7d375edbe467e73b0d1084f1adb12c653cf57f31
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-09-19 20:47:43 +0200

    navbar update

--- app/components/layout/navbar/Navbar.tsx
+++ app/components/layout/navbar/Navbar.tsx
@@ -4,25 +4,48 @@ import { ModeToggle } from "../../header/provider/Button";
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 import { Socials } from "./components/socials/Socials";
+import { usePathname } from "next/navigation";
+import { Prompt } from "next/font/google";
+const prompt = Prompt({
+  subsets: ["latin"],
+  weight: "800",
+});
 const Navbar = () => {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+
   return (
-    <div className="sticky top-5 z-10 ">
+    <div className={`sticky top-5 z-10 ${prompt.className}`}>
       <div className="flex flex-row justify-between">
         <ModeToggle />
         <ul className="flex flex-row items-baseline">
-          <li className="mx-10 flex flex-row">
-            {" "}
+          <li className="mx-20 flex flex-row">
             <Link href="/">
-              <FaHome className="text-emerald-400" size={25} />
+              <FaHome
+                className={`white ${isActive("/") ? "text-emerald-400" : ""}`}
+                size={25}
+              />
             </Link>
           </li>
           <li className="mx-5">
-            {" "}
-            <Link href="/pages/about">about</Link>
+            <Link
+              href="/pages/about"
+              className={
+                isActive("/pages/about") ? "text-emerald-400 underline" : ""
+              }
+            >
+              about
+            </Link>
           </li>
           <li className="mx-5">
-            {" "}
-            <Link href="/pages/projects">projects</Link>
+            <Link
+              href="/pages/projects"
+              className={
+                isActive("/pages/projects") ? "text-emerald-400 underline" : ""
+              }
+            >
+              projects
+            </Link>
           </li>
           <Socials />
         </ul>

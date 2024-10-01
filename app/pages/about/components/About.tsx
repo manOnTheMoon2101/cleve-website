c1f3253e96c032a7af01b3fec75a90a505a7877e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-10-01 21:17:55 +0200

    about update

--- app/pages/about/components/About.tsx
+++ app/pages/about/components/About.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "../components/styles.module.css";
 import { Prompt } from "next/font/google";
-import sa from "../../../../public/icons/sa.svg";
-import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 const prompt = Prompt({
   subsets: ["latin"],
   weight: "800",
@@ -24,36 +28,39 @@ const AboutContent = () => {
           />
         </div>
       </div>
-      <div className="flex flex-row justify-center">
-        <div className="p-10">
-          <h2
-            className={`text-6xl text-center md:text-9xl font-bold text-center tracking-wide ${prompt.className}`}
-          >
-            npm <span className="text-emerald-400 italic underline">init</span>?
-          </h2>
-        </div>
-      </div>
       <div className={`${prompt.className} m-5 text-6xl`}>
         <p className="text-left">
           my name's cleve,
           <br />i make apps for a living
         </p>
         <p className="my-40 text-right">
-          falled in love with tech at a young age
+          falled in l❤️ve with tech at a young age
           <br />
-          (since i was chewing batteries as a baby)
+          <span className="text-4xl text-slate-400">
+            (since i was chewing batteries as a baby)
+          </span>
         </p>
         <p className="text-left">
-          "Plat op die aarde" christian guy
+         <span><TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                  <span>"Plat op die aarde"</span>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>down to earth</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider></span> christian guy
           <br />
           who enjoys the little things in life
         </p>
-        <p className="my-40 text-right">
+        <p className="my-40 text-center text-4xl text-emerald-400">
           "It is like a mustard seed, which is the smallest of all seeds on
           earth.
           <br />
           Yet when planted, it grows and becomes the largest of all garden
-          plants, with such big branches that the birds can perch in its shade”
+          plants, with such big branches that the birds can perch in its shade”-
+          <span className="text-4xl text-slate-400">Mark 4-31</span>
         </p>
       </div>
     </>

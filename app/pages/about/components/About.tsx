425260db0b0457d3b8b879c0cda4559a7fc6584e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-01-16 21:30:31 +0200

    about page update

--- app/pages/about/components/About.tsx
+++ app/pages/about/components/About.tsx
@@ -14,6 +14,8 @@ import beach from "@/public/images/beach.jpeg";
 import food from "@/public/images/food.jpeg";
 import cleveC from "@/public/images/cleveC.jpeg";
 import { Spline_Sans_Mono } from "next/font/google";
+import { InView } from "@/components/ui/in-view";
+import { InViewBasic } from "./Body/Body";
 const prompt = Prompt({
   subsets: ["latin"],
   weight: "800",
@@ -37,49 +39,10 @@ const AboutContent = () => {
           />
         </div>
       </div>
-      <div className={`${spline.className} my-40 text-6xl`}>
-        <p className="text-center md:text-left">
-          My name's <span className="text-emerald-400 italic">@cleve</span>,
-          <br />
-          I'm a <span className="underline">Software Developer</span>
-        </p>
-        <p className="my-40 text-center md:text-right">
-          I fell in L❤️ve with Tech at a young age
-          <br />
-          <span className="text-4xl text-slate-400 italic">
-            (since i was chewing batteries as a baby)
-          </span>
-        </p>
-        <p className="text-center md:text-left my-40 ">
-          <span>
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <span className="">"Plat op die aarde"</span>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Down to earth</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </span>{" "}
-          Christian guy
-          <br />
-          who enjoys the{" "}
-          <span className="text-2xl italic text-yellow-900">little</span> things
-          in life.
-        </p>
-        <p className="my-40 text-center text-4xl text-emerald-400">
-          "It is like a mustard seed, which is the smallest of all seeds on
-          earth.
-          <br />
-          Yet when planted, it grows and becomes the largest of all garden
-          plants, with such big branches that the birds can perch in its shade”-
-          <span className="text-4xl text-slate-400">Mark 4-31</span>
-        </p>
-        <div className="flex flex-row justify-center">
-          <Image src={sign} alt="singnature" />
-        </div>
+      <InViewBasic />
+
+      <div className="flex flex-row justify-center">
+        <Image src={sign} alt="singnature" />
       </div>
     </>
   );
---
+++ app/pages/about/components/Body/Body.tsx
@@ -0,0 +1,95 @@
+import { InView } from "@/components/ui/in-view";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Spline_Sans_Mono } from "next/font/google";
+const spline = Spline_Sans_Mono({
+  subsets: ["latin"],
+});
+export function InViewBasic() {
+  return (
+    <div className={`${spline.className} my-40 text-6xl`}>
+      <InView
+        variants={{
+          hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+          visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+        }}
+        viewOptions={{ margin: "0px 0px -200px 0px" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        <p className="text-center md:text-left">
+          My name's <span className="text-emerald-400 italic">@cleve</span>,
+          <br />
+          I'm a <span className="underline">Software Developer</span>
+        </p>
+      </InView>
+
+      <InView
+        variants={{
+          hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+          visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+        }}
+        viewOptions={{ margin: "0px 0px -200px 0px" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        <p className="my-40 text-center md:text-right">
+          I fell in L❤️ve with Tech at a young age
+          <br />
+          <span className="text-4xl text-slate-400 italic">
+            (since i was chewing batteries as a baby)
+          </span>
+        </p>
+      </InView>
+
+      <InView
+        variants={{
+          hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+          visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+        }}
+        viewOptions={{ margin: "0px 0px -200px 0px" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        <p className="text-center md:text-left my-40 ">
+          <span>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <span className="">"Plat op die aarde"</span>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Down to earth</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </span>{" "}
+          Christian guy
+          <br />
+          who enjoys the{" "}
+          <span className="text-2xl italic text-yellow-900">little</span> things
+          in life.
+        </p>
+      </InView>
+
+      <InView
+        variants={{
+          hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+          visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+        }}
+        viewOptions={{ margin: "0px 0px -200px 0px" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        <p className="my-40 text-center text-4xl text-emerald-400">
+          "It is like a mustard seed, which is the smallest of all seeds on
+          earth.
+          <br />
+          Yet when planted, it grows and becomes the largest of all garden
+          plants, with such big branches that the birds can perch in its shade”-
+          <span className="text-4xl text-slate-400">Mark 4-31</span>
+        </p>
+      </InView>
+    </div>
+  );
+}

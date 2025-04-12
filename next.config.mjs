f066bde68cef8a9305e93c2c13d0d7462edeb8a3
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-04-12 15:31:52 +0200

    again

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        // your remote patterns if any
-      ],
-    },
-    // Add this to ensure proper Sharp compatibility
-    webpack: (config) => {
-      config.externals = [...(config.externals || []), "sharp"];
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;

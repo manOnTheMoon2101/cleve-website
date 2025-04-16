7bec05274764459d6f523921b22e6bd0464e7693
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-04-16 19:06:29 +0200

    to drawer

--- app/components/body/components/socials/Socials.tsx
+++ app/components/body/components/socials/Socials.tsx
@@ -33,98 +33,6 @@ const spline = Spline_Sans_Mono({
 export function Socials() {
   const [open, setOpen] = React.useState(false);
   const isDesktop = useMediaQuery("(min-width: 768px)");
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button variant={"outline"} size="icon">
-            <MessageCircle />
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <div className="mt-10">
-            <DialogTitle className={`text-center text-4xl ${spline.className}`}>
-              Say Hey!
-            </DialogTitle>
-            <div className="flex flex-row justify-center m-10">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <a
-                      href="https://www.linkedin.com/in/cleve-clayton/"
-                      target="_blank"
-                      className="px-5"
-                      rel="noopener noreferrer"
-                    >
-                      <div className="flex items-center text-gray-700 font-neucha text-base leading-6 py-3 px-3 rounded-custom  transition-all duration-235 ease-in-out cursor-pointer select-none touch-manipulation transform hover:translate-y-0.5 focus:ring-0 focus:shadow-lg focus:translate-y-0.5">
-                        <Image
-                          src={linkedin}
-                          alt={"Linkedin"}
-                          height={60}
-                          width={60}
-                        />
-                      </div>
-                    </a>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Linkedin</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <a
-                      href="https://github.com/manOnTheMoon2101"
-                      target="_blank"
-                      className="px-5"
-                      rel="noopener noreferrer"
-                    >
-                      <div className="flex text-white items-center font-neucha text-base leading-6 py-3 px-3  rounded-custom  transition-all duration-235 ease-in-out cursor-pointer select-none touch-manipulation transform hover:translate-y-0.5 focus:ring-0 focus:shadow-lg focus:translate-y-0.5">
-                        <Image
-                          src={github}
-                          alt={"Github"}
-                          height={60}
-                          width={60}
-                        />
-                      </div>
-                    </a>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Github</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <a
-                      href="mailto:clevejohnclayton.2101@gmail.com"
-                      target="_blank"
-                      className="px-5"
-                      rel="noopener noreferrer"
-                    >
-                      <div className="flex items-center text-gray-700 font-neucha text-base leading-6 py-3 px-3 rounded-custom  transition-all duration-235 ease-in-out cursor-pointer select-none touch-manipulation transform hover:translate-y-0.5 focus:ring-0 focus:shadow-lg focus:translate-y-0.5">
-                        <Image
-                          src={email}
-                          alt={"Email"}
-                          height={60}
-                          width={60}
-                        />
-                      </div>
-                    </a>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Email : clevejohnclayton.2101@gmail.com</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
-    );
-  }
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
@@ -134,9 +42,9 @@ export function Socials() {
       </DialogTrigger>
       <DrawerContent>
         <div className="mt-10">
-          <h2 className={`text-center text-4xl ${spline.className}`}>
+          <DialogTitle className={`text-center text-4xl ${spline.className}`}>
             Say Hey!
-          </h2>
+          </DialogTitle>
           <div className="flex flex-row justify-center m-10">
             <TooltipProvider>
               <Tooltip>

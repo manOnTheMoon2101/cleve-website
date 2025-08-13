95b66f86ad1a373c421af5ada65303b18a464a03
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-08-13 11:00:50 +0200

    link add

--- app/projects/components/Projects.tsx
+++ app/projects/components/Projects.tsx
@@ -111,8 +111,9 @@ const ProjectsContent = () => {
       body: "A weight tracker app that displays your main nutrients through advanced, visually stunning graphs and detailed table for comprehensive health tracking.",
     },
     weight2: {
-      title: "Health Tracker App V2",
+      title: "Health Tracker App V2(Beta)",
       github: "https://github.com/manOnTheMoon2101/weight-managementV2",
+      link:"https://weight-management-v2.vercel.app/",
       stack: [
         {
           icon: <RiSvelteFill color="#ff5805" />,
